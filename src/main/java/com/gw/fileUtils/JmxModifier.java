9346fad836f9a64cca80dc4791fe77e76992873a
Eason.ZHANG <zhyj663@163.com>
2017-08-08 16:35:36 +0800

    首次提交
    Signed-off-by: Eason.ZHANG <zhyj663@163.com>

---
+++ src/main/java/com/gw/fileUtils/JmxModifier.java
@@ -0,0 +1,16 @@
+package com.gw.fileUtils;
+
+public class JmxModifier {
+	private String strPath;
+	
+	
+	public JmxModifier(){
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
